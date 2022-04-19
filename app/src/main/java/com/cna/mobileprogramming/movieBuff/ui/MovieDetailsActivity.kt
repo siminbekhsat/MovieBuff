56c9142a0825cd6deaafea4636dce5ed6c1b127c
Simin Bekhsat <56419294+siminbekhsat@users.noreply.github.com>
2022-04-19 11:47:49 -0230

    added Details screen
    contains an image container and 2 textViews

---
+++ app/src/main/java/com/cna/mobileprogramming/movieBuff/ui/MovieDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.cna.mobileprogramming.movieBuff.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cna.mobileprogramming.foodie.R
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
