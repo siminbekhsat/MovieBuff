c854e5491e43008ed133e4b3bcf98991497967e6
Simin Bekhsat <56419294+siminbekhsat@users.noreply.github.com>
2022-04-19 11:56:24 -0230

    Added Phone Number Field
    added phone number field to Movie object with default value

--- app/src/main/java/com/cna/mobileprogramming/movieBuff/model/Model.kt
+++ app/src/main/java/com/cna/mobileprogramming/movieBuff/model/Model.kt
@@ -1,3 +1,8 @@
 package com.cna.mobileprogramming.movieBuff.model
 
-data class Movie(val name: String, val imageUrl: String, val category: String)
+data class Movie(
+    val name: String,
+    val imageUrl: String,
+    val category: String,
+    val phoneNumber: String = "709-123-4567"
+)
